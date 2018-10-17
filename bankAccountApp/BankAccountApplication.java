f6253bddf7cff9f7c85ebbb025935984c1c60540
Joseph Mendez <13544990+Joseph-Mendez@users.noreply.github.com>
2018-10-17 08:56:07 -0700

    Update BankAccountApplication.java

--- bankAccountApp/BankAccountApplication.java
+++ bankAccountApp/BankAccountApplication.java
@@ -7,6 +7,9 @@ public class BankAccountApplication {
 		Checking cheacc1 = new Checking("Joseph Mendez", "123456789", 1500);
 		Savings savacc1 = new Savings("Joseph Mendez", "987654321", 9000);
 		
+		savacc1.showInfo();
+		System.out.println("************");
+		chkacc1.showInfo();
 
 	}
 
